fa346d943325f23cb6664074875aa20686769aae
Jonathan Ferreira <j0n4t@hotmail.com>
2024-03-14 21:48:36 -0300

    init: backend

---
+++ backend/src/main/java/com/testuol/clients/backend/BackendApplication.java
@@ -0,0 +1,15 @@
+package com.testuol.clients.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The main class for the backend application.
+ */
+@SpringBootApplication
+public class BackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BackendApplication.class, args);
+  }
+}
