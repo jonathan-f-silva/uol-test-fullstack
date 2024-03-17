1279bf4a48f69ac187f718048e261a2b82baeac7
Jonathan Ferreira <j0n4t@hotmail.com>
2024-03-17 05:01:22 -0300

    tests(frontend): better mocks

--- frontend/src/tests/mocks.ts
+++ frontend/src/tests/mocks.ts
@@ -1,10 +1,34 @@
 export const mockCustomers: Customer[] = [
   {
     id: 1,
-    name: "user1",
+    name: "John Doe 1",
     email: "user1@mail.com",
-    phone: "11922223333",
+    phone: "(11) 2222-3333",
     cpf: "123.456.789-01",
     status: "ACTIVE",
+  },
+  {
+    id: 2,
+    name: "John Doe 2",
+    email: "user2@mail.com",
+    phone: "(22) 2222-3333",
+    cpf: "123.456.789-02",
+    status: "INACTIVE",
+  },
+  {
+    id: 3,
+    name: "John Doe 3",
+    email: "user3@mail.com",
+    phone: "(33) 2222-3333",
+    cpf: "123.456.789-03",
+    status: "PENDING",
+  },
+  {
+    id: 4,
+    name: "John Doe 4",
+    email: "user2@mail.com",
+    phone: "(44) 2222-3333",
+    cpf: "123.456.789-04",
+    status: "DISABLED",
   },
 ];
