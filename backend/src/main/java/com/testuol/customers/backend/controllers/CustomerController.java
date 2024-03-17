41c63f85043a9b4a7c5e487033143030c35b36fd
Jonathan Ferreira <j0n4t@hotmail.com>
2024-03-17 06:04:30 -0300

    tweak(backend): main endpoint

--- backend/src/main/java/com/testuol/customers/backend/controllers/CustomerController.java
+++ backend/src/main/java/com/testuol/customers/backend/controllers/CustomerController.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
  * Controller for customer operations.
  */
 @RestController
-@RequestMapping("/customer")
+@RequestMapping("/customers")
 public class CustomerController {
   @Autowired
   CustomerService customerService;
