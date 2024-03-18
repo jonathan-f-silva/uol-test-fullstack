d929e1f67a53bdfa7656956aef8238c48b792593
Jonathan Ferreira <j0n4t@hotmail.com>
2024-03-18 14:28:46 -0300

    feat(frontend): custom theme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,22 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { routes } from "./routes";
 import { CustomerProvider } from "./contexts/CustomerContext";
 
+const theme = extendTheme({
+  components: {
+    Button: {
+      defaultProps: {
+        colorScheme: "orange",
+      },
+    },
+  },
+});
+
 export function App() {
   return (
     <CustomerProvider>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <RouterProvider router={createBrowserRouter(routes)} />
       </ChakraProvider>
     </CustomerProvider>
