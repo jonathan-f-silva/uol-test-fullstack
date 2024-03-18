8c6bedda9dc61847f82be63e8431faff8a088a54
Jonathan Ferreira <j0n4t@hotmail.com>
2024-03-18 14:19:00 -0300

    feat(frontend): Logo, favicon and header

--- frontend/src/components/AppLayout.tsx
+++ frontend/src/components/AppLayout.tsx
@@ -1,11 +1,35 @@
-import { Heading, Stack } from "@chakra-ui/react";
+import {
+  Container,
+  Heading,
+  Icon,
+  Image,
+  Stack,
+  VStack,
+} from "@chakra-ui/react";
+import { MdOutlinePeopleAlt } from "react-icons/md";
+
 import { Outlet } from "react-router-dom";
 
 export function AppLayout() {
   return (
-    <Stack spacing={4}>
-      <Heading as="h1">Painel de Clientes</Heading>
-      <Outlet />
-    </Stack>
+    <>
+      <VStack
+        as="header"
+        height="60px"
+        backgroundColor="#333"
+        justifyContent="center"
+      >
+        <Image src="./uol-logo.png" alt="Logo do UOL" height="32px" />
+      </VStack>
+      <Container maxWidth="80%" py={8}>
+        <Stack spacing={4}>
+          <Heading as="h1">
+            <Icon as={MdOutlinePeopleAlt} marginRight={4} />
+            Painel de Clientes
+          </Heading>
+          <Outlet />
+        </Stack>
+      </Container>
+    </>
   );
 }
